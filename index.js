908ffaf6bd029841303a62494b9d8696452b4b56
Gerard Mistretta <gerardmistretta@yahoo.com>
2020-10-12 17:37:00 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 document.addEventListener("DOMContentLoaded", function() {
     console.log("The DOM has loaded");
   })
-  
+
 
   //let text = document.getElementById("text");
   //text.innerHTML = "This is really cool!"
